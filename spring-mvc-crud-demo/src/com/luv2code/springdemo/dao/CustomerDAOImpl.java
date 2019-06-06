89a1b93be988a2d3204513a35c3b89bb601845b6
dHARMU <dHARMU@DESKTOP-BITF6J7>
2019-06-07 00:17:51 +0530

    emusicStore working

--- spring-mvc-crud-demo/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ spring-mvc-crud-demo/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -64,10 +64,10 @@ public class CustomerDAOImpl implements CustomerDAO {
 		Session currentSession = sessionFactory.getCurrentSession();
 
 		// delete object with primary key
-//		Query theQuery = currentSession.createQuery("delete from Customer where id=:customerId");
-//		theQuery.setParameter("customerId", theId);
-//
-//		theQuery.executeUpdate();
+		Query theQuery = currentSession.createQuery("delete from Customer where id=:customerId");
+		theQuery.setParameter("customerId", theId);
+
+		theQuery.executeUpdate();
 	}
 
 }
